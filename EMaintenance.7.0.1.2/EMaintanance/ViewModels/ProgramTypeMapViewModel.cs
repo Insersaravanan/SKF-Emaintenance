7e126a5094080a0e7277277d188a42ebe12744b8
Insersaravanan <68779765+Insersaravanan@users.noreply.github.com>
2020-08-03 10:10:04 +0530

    Add files via upload

---
+++ EMaintenance.7.0.1.2/EMaintanance/ViewModels/ProgramTypeMapViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMaintanance.ViewModels
+{
+    public class ProgramTypeMapViewModel
+    {
+        public int PTMappingId { get; set; }
+        public int PTMappingTId { get; set; }
+        public int ProgramTypeId { get; set; }
+        public int ClientSiteId { get; set; }
+        public int LanguageId { get; set; }
+        public int UserId { get; set; }
+        public string Active { get; set; }
+    }
+}
