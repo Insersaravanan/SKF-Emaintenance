b7746dd0b242c8eaeae651ccee850ac8436939cb
mksaran <mksaran@insersolutions.com>
2020-08-07 01:36:00 -0700

    Emaint

---
+++ EMaintenance.7.0.1.2/EMaintanance/Controllers/ProgramTypeMapController.cs
@@ -0,0 +1,144 @@
+﻿using EMaintanance.Repository;
+using EMaintanance.Utils;
+using EMaintanance.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EMaintanance.Controllers
+{
+    [SKFAuthorize("PRG70")]
+    public class ProgramTypeMapController : Controller
+    {
+        private readonly ProgramTypeMappingRepo ptmRepo;
+        private IConfiguration _configuration;
+        public ProgramTypeMapController(IConfiguration configuration)
+        {
+            ptmRepo = new ProgramTypeMappingRepo(configuration);
+            _configuration = configuration;
+        }
+
+        [SKFAuthorize("PRG70:P1")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [SKFAuthorize("PRG70:P1")]
+        public async Task<IActionResult> Get(int lId, int csId)
+        {
+            try
+            {
+                return Ok(await ptmRepo.GetProgramTypeMapping(lId, csId));
+            }
+            catch (CustomException cex)
+            {
+                var responseObj = new EmaintenanceMessage(cex.Message, cex.Type, cex.IsException, cex.Exception?.ToString());
+                return StatusCode(StatusCodes.Status500InternalServerError, responseObj);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new EmaintenanceMessage(ex.Message));
+            }
+        }
+
+        [HttpGet]
+        [SKFAuthorize("PRG70:P1")]
+        public async Task<IActionResult> GetProgramTypeSetup(int lId, int csId)
+        {
+            try
+            {
+                return Ok(await ptmRepo.GetProgramTypeSetup(lId, csId));
+            }
+            catch (CustomException cex)
+            {
+                var responseObj = new EmaintenanceMessage(cex.Message, cex.Type, cex.IsException, cex.Exception?.ToString());
+                return StatusCode(StatusCodes.Status500InternalServerError, responseObj);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new EmaintenanceMessage(ex.Message));
+            }
+        }
+
+        [HttpPost]
+        [SKFAuthorize("PRG70:P2")]
+        public async Task<IActionResult> Create([FromBody] List<ProgramTypeMapViewModel> ptmvms)
+        {
+            try
+            {
+                CurrentUser cUser = new CurrentUser(HttpContext, _configuration);
+                foreach (ProgramTypeMapViewModel ptmvm in ptmvms)
+                {
+                    //   if (ptmvm.ProgramTypeId != null && ptmvm.Active== "Y")
+                    if (ptmvm.ProgramTypeId != null)
+                    {
+                        ptmvm.UserId = cUser.UserId;
+                        ptmvm.ProgramTypeId = ptmvm.ProgramTypeId;
+                        await ptmRepo.SaveOrUpdate(ptmvm);
+                    }
+                    //else
+                    //{
+                    //    throw new CustomException("Mandatory", "Client ProgramType Name is Mandatory.", "Error", true, "Custom Message : Client ProgramType Name is Mandatory.");
+                    //}
+                }
+                return Ok();
+
+            }
+            catch (CustomException cex)
+            {
+                var returnObj = new EmaintenanceMessage(cex.Message, cex.Type, cex.IsException, cex.Exception?.ToString());
+                return StatusCode(StatusCodes.Status500InternalServerError, returnObj);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new EmaintenanceMessage(ex.Message));
+            }
+        }
+
+        [HttpGet]
+        [SKFAuthorize("PRG70:P4")]
+        public async Task<IActionResult> GetTransProgramTypeMapping(int CMappingId)
+        {
+            try
+            {
+                return Ok(await ptmRepo.GetTransProgramTypeMapping(CMappingId));
+            }
+            catch (CustomException cex)
+            {
+                var responseObj = new EmaintenanceMessage(cex.Message, cex.Type, cex.IsException, cex.Exception?.ToString());
+                return StatusCode(StatusCodes.Status500InternalServerError, responseObj);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new EmaintenanceMessage(ex.Message));
+            }
+        }
+
+        [HttpGet]
+        [SKFAuthorize("PRG70:P4")]
+        public async Task<IActionResult> GetProgramTypeByClientID(int lId, int csId, string lName)
+        {
+            try
+            {
+                return Ok(await ptmRepo.GetProgramTypeByClientID(lId, csId, lName));
+            }
+            catch (CustomException cex)
+            {
+                var responseObj = new EmaintenanceMessage(cex.Message, cex.Type, cex.IsException, cex.Exception?.ToString());
+                return StatusCode(StatusCodes.Status500InternalServerError, responseObj);
+            }
+            catch (Exception ex)
+            {
+                return Ok(new EmaintenanceMessage(ex.Message));
+            }
+        }
+
+
+    }
+}
---
+++ EMaintenance.7.0.1.2/EMaintanance/obj/Release/netcoreapp2.1/PubTmp/Out/wwwroot/js/userguide/index.js
@@ -0,0 +1,35 @@
+﻿app.requires.push('commonMethods', 'ngTouch', 'ui.grid', 'ui.grid.selection', 'ui.grid.resizeColumns', 'ui.grid.edit', 'ui.grid.cellNav', 'ui.grid.pinning', 'ui.grid.exporter', 'ui.bootstrap', 'ui.grid.selection');
+
+app.controller('skfCtrl', function ($scope, $filter, uiGridConstants, $http, $uibModal,$translate,$rootScope,languageFactory, apiFactory, alertFactory, $timeout, $sce) {
+    $scope.loadDocumentation = function () {
+        var _url = "/Lookup/GetLookupByName?lId=" + $scope.language.LanguageId + "&lName=Documentation";
+        $http.get(_url)
+            .then(function (response) {
+                $scope.DocumentationDDL = response.data;
+            });
+    };
+
+    $scope.$watch(function () {
+        return languageFactory.getLanguage();
+    }, function (newValue, oldValue) {
+        if (newValue != oldValue && newValue) {
+            $scope.language = newValue;
+            $rootScope.lang = $scope.language.CountryCode;
+            $translate.use($scope.language.CountryCode);
+            $scope.loadDocumentation();
+        }
+    });
+
+    $scope.ViewDocument = function (data, index) {
+        $scope.docindex = index;
+        $http.get("/PageSetup/GetContentByTypeCode?typeCode=" + data).then(function (response) {
+            if (response.data) {
+                $scope.document = $sce.trustAsResourceUrl(response.data.typeName);
+            }
+        });
+    };
+    var tabs = angular.module('tabs', [])
+        .controller('tabCtrl', function ($scope) {
+            $scope.selected = "1";
+        });
+});
